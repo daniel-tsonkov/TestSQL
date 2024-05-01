84048b76d9c676f3465626f63fc7379c25583ce8
Daniel <daniel_tzonkov@abv.bg>
2024-05-01 17:48:10 +0300

    01052024

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//import com.sun.jdi.connect.spi.Connection;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, IOException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "test");
+        properties.setProperty("password", "aassdd");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.5.200:3306/minions_db", properties);
+
+
+        connection.close();
+    }
+
+}
