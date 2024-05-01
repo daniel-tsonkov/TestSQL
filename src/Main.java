52c509350aca92d420d93bdd7cc5f9d6875c6e09
Daniel <daniel_tzonkov@abv.bg>
2024-05-01 18:19:18 +0300

    01052024

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException {
         Properties properties = new Properties();
         properties.setProperty("user", "test");
         properties.setProperty("password", "aassdd");
