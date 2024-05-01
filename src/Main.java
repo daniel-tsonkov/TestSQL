a97ee25113386b67a97d94152f5401eb2685ad2e
Daniel <daniel_tzonkov@abv.bg>
2024-05-01 19:10:54 +0300

    01052024

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,19 @@ public class Main {
 
         Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.5.200:3306/minions_db", properties);
 
-        PreparedStatement statement = connection.prepareStatement("SELECT * FROM villains");
+        PreparedStatement statement = connection.prepareStatement("" +
+                "SELECT name, COUNT(mv.minion_id) as minion_count from villains as v " +
+                "JOIN minions_villains as mv on mv.villain_id = v.id " +
+                "GROUP by mv.villain_id " +
+                "HAVING minion_count > 15 " +
+                "ORDER  BY minion_count DESC;");
         ResultSet resultSet = statement.executeQuery();
 
         while (resultSet.next()) {
             String villainName = resultSet.getString("name");
+            int minionCount = resultSet.getInt("minion_count");
 
-            System.out.println(villainName);
+            System.out.println(villainName + " " + minionCount);
         }
 
 
