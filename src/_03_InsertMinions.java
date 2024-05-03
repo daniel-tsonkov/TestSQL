c8bfb3349f1163ec9b53482b8ca8da579492cf4e
Daniel <daniel_tzonkov@abv.bg>
2024-05-03 15:59:43 +0300

    03052024

--- src/_03_InsertMinions.java
+++ src/_03_InsertMinions.java
@@ -12,9 +12,32 @@ public class _03_InsertMinions {
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] minionInfo = scanner.nextLine().split(" ");
-        String[] villainInfo = scanner.nextLine().split(" ");
+        /*Minions: Robert 14 Berlin
+          Villains: Gru*/
 
+        String[] minionInfo = scanner.nextLine().split(" ");
+        String minionName = minionInfo[1];
+        int minionAge = Integer.parseInt(minionInfo[2]);
+        String minionTown = minionInfo[3];
+
+        String villainName = scanner.nextLine().split(" ")[1];
+
+        PreparedStatement selectTown = connection.prepareStatement("SELECT id FROM towns WHERE name = ?");
+        selectTown.setString(1, minionTown);
+        ResultSet townSet = selectTown.executeQuery();
+
+        int townId = 0;
+        if (!townSet.next()) {
+            PreparedStatement insertTown = connection.prepareStatement("INSERT INTO towns(name) VALUES (?);");
+
+            insertTown.setString(1, minionTown);
+            ResultSet newTownSet = selectTown.executeQuery();
+            townId = newTownSet.getInt("id");
+        } else {
+            townId = townSet.getInt("id");
+        }
+
+        System.out.println(townId);
 
         connection.close();
     }
